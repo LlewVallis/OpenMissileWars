54c5de195dc8081faf3d75637f859e6038358e43
LlewVallis <llewvallis@gmail.com>
2020-07-06 17:38:48 +1000

    Build plugin in start script

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+echo "Downloading artifacts"
 cd artifacts
 ./setup.sh
 cd ..
 
+echo "Building plugin"
+cd plugin
+mvn package
+cd ..
+
+echo "Executing server"
 cd server
 ./start.sh
